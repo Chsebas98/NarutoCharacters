d5b6f780f3327604a431a173702aa17c62810743
Chsebas98 <chsebas98@gmail.com>
2021-06-10 17:06:36 -0500

    algun cambio

--- src/Pages/ManScreen.jsx
+++ src/Pages/ManScreen.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { Characters } from "../Models/Characters";
 
 const ManScreen = () => {
+  const mans = Characters.filter((personaje) => personaje.type === "h");
+  console.log(mans);
   return (
-    <div>
+    <div className="container mt-3">
       <h1>Mans</h1>
+      <hr></hr>
+      {mans.map((personajes) => (
+        <h1>{personajes.name}</h1>
+      ))}
     </div>
   );
 };
